54ca179e397f00b7f55f31dffadfe2842a81951e
sakai-memore <mitsuru.sakai@gmail.com>
2022-03-14 10:18:17 +0900

    Implement es modules on bundling to work for properties panel module

--- webapp/static/bpmn-modeler-customed/src/properties.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-  $(".toggle-panel").on("click", () => {
-    $("#properties-panel-parent").toggleClass("collapsed");
-    $("#js-canvas").toggleClass("col-md-12 col-md-8");
-    $("#btn-toggle > i").toggleClass("bi bi-toggle-on bi bi-toggle-off");
-    // console.log("click toggle button!");
-    return false;
-  });
-});
